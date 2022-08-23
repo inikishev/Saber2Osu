0dcf25ff13be346676db924344576edb7cdd2d9f
idkman2021 <76593873+idkman2021@users.noreply.github.com>
2022-08-23 20:03:35 +0300

    Add files via upload

--- converter.py
+++ converter.py
@@ -1,8 +1,16 @@
 # it needs to just not work when file exists
 import os
 
-
-
+from tkinter.filedialog import askopenfilename
+filename = askopenfilename()
+pathname= filename[:filename.rfind("/")]
+import random
+num = random.random()
+tempname= pathname + "/BeatSaberToOsuConverter" + str(num)
+import zipfile
+with zipfile.ZipFile(filename,"r") as zip_ref:
+    zip_ref.extractall(tempname)
+os.chdir(tempname)
 def convert(beatspermin, bstiming):
    unit = float(60 / float(beatspermin) / 8)
    stuff = float(bstiming)*8
@@ -17,7 +25,7 @@ songName = (info[info.find("_songName")+13:info.find("_songSubName")-4])
 songAuthorName = (info[info.find("_songAuthorName")+19:info.find("_levelAuthorName")-4])
 levelAuthorName = (info[info.find("_levelAuthorName")+20:info.find("_beatsPerMinute")-4])
 bpm = (info[info.find("_beatsPerMinute")+18:info.find("_songTimeOffset")-3])
-
+FileNamer = songAuthorName + " - " + songName + " (" + levelAuthorName + ")"
 print("Song name: " + songName + "\nSong author: " + songAuthorName + "\nBeatmap author: " + levelAuthorName + "\nBPM: " + bpm)
 
    
@@ -55,7 +63,7 @@ def difficulty(filename, HPDrainRate, CircleSize, OverallDifficulty, ApproachRat
    # bpms = float(bpm)/60000
    # print(bpms)
    # the beginning of the osu file with dumb parameters but idk how to change them so circles are really slow and dumb and i dont like them
-   osu = "osu file format v14\n\n[General]\nAudioFilename: audio.mp3\nAudioLeadIn: 0\nPreviewTime: -1\nCountdown: 0\nSampleSet: Normal\nStackLeniency: 0.7\nMode: 0\nLetterboxInBreaks: 0\nWidescreenStoryboard: 0\n\n[Editor]\nDistanceSpacing: 1.1\nBeatDivisor: 4\nGridSize: 8\nTimelineZoom: 1.6\n\n[Metadata]\nTitle:" + songName + "\nTitleUnicode:" + songName +"\nArtist:" + songAuthorName + "\nArtistUnicode:" + songAuthorName + "\nCreator:" + levelAuthorName + "\nVersion:" + filename + "\nSource:\nTags:BeatSaber\nBeatmapID:0\nBeatmapSetID:-1\n\n[Difficulty]\nHPDrainRate:" + str(HPDrainRate) + "\nCircleSize:" + str(CircleSize) + "\nOverallDifficulty:" + str(OverallDifficulty) + "\nApproachRate:" + str(ApproachRate) + "\nSliderMultiplier:" + str(SliderMultiplier) + "\nSliderTickRate:" + str(SliderTickRate) + "\n\n[Events]\n//Background and Video events\n//Break Periods\n//Storyboard Layer 0 (Background)\n//Storyboard Layer 1 (Fail)\n//Storyboard Layer 2 (Pass)\n//Storyboard Layer 3 (Foreground)\n//Storyboard Layer 4 (Overlay)\n//Storyboard Sound Samples\n\n[TimingPoints]\n0,352.941176470588,4,1,0,100,1,0\n\n\n[HitObjects]\n"
+   osu = "osu file format v14\n\n[General]\nAudioFilename: song.ogg\nAudioLeadIn: 0\nPreviewTime: -1\nCountdown: 0\nSampleSet: Normal\nStackLeniency: 0.7\nMode: 0\nLetterboxInBreaks: 0\nWidescreenStoryboard: 0\n\n[Editor]\nDistanceSpacing: 1.1\nBeatDivisor: 4\nGridSize: 8\nTimelineZoom: 1.6\n\n[Metadata]\nTitle:" + songName + "\nTitleUnicode:" + songName +"\nArtist:" + songAuthorName + "\nArtistUnicode:" + songAuthorName + "\nCreator:" + levelAuthorName + "\nVersion:" + filename + "\nSource:\nTags:BeatSaber\nBeatmapID:0\nBeatmapSetID:-1\n\n[Difficulty]\nHPDrainRate:" + str(HPDrainRate) + "\nCircleSize:" + str(CircleSize) + "\nOverallDifficulty:" + str(OverallDifficulty) + "\nApproachRate:" + str(ApproachRate) + "\nSliderMultiplier:" + str(SliderMultiplier) + "\nSliderTickRate:" + str(SliderTickRate) + "\n\n[Events]\n//Background and Video events\n//Break Periods\n//Storyboard Layer 0 (Background)\n//Storyboard Layer 1 (Fail)\n//Storyboard Layer 2 (Pass)\n//Storyboard Layer 3 (Foreground)\n//Storyboard Layer 4 (Overlay)\n//Storyboard Sound Samples\n\n[TimingPoints]\n0,352.941176470588,4,1,0,100,1,0\n\n\n[HitObjects]\n"
 
    for i in range(len(time)):
       osu = osu + str(int(bs[lineIndex[i]+13])*100) + ',' + str(int(bs[lineLayer[i]+13])*100) + ',' + str(convert(bpm, bs[(time[i]+8):(time[i]+24)])) + ',1,0,0:0:0:0:\n'
@@ -86,3 +94,11 @@ print(difficulty('360DegreeNormal', 5, 3.5, 6, 8, 1.4, 1))
 print(difficulty('360DegreeHard', 5, 4.4, 8, 9, 2.5, 1))
 print(difficulty('360DegreeExpert', 5.5, 4.8, 8.5, 9, 3.2, 1))
 print(difficulty('360DegreeExpertPlus', 5.6, 4.9, 8.6, 9, 3.3, 1))
+
+os.chdir(pathname)
+import shutil
+shutil.make_archive(FileNamer, 'zip', tempname)
+from pathlib import Path
+p = Path(pathname + '/' + FileNamer + '.zip')
+p.rename(p.with_suffix('.osz'))
+shutil.rmtree(tempname)
